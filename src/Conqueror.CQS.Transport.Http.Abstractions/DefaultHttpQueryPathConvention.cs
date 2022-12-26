7d70fc587e4a7d9612b119fb16b66943ba8682ea
Jonathan Ziller <jonathan.ziller@gmail.com>
2022-12-26 15:22:11 +0100

    performance(cqs.transport.http): cache paths in default path conventions

--- src/Conqueror.CQS.Transport.Http.Abstractions/DefaultHttpQueryPathConvention.cs
+++ src/Conqueror.CQS.Transport.Http.Abstractions/DefaultHttpQueryPathConvention.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Text.RegularExpressions;
 
 namespace Conqueror
@@ -6,6 +7,7 @@ namespace Conqueror
     public sealed class DefaultHttpQueryPathConvention : IHttpQueryPathConvention
     {
         private static readonly Regex StripSuffixRegex = new("Query$");
+        private static readonly ConcurrentDictionary<Type, string> PathCache = new();
 
         public string GetQueryPath(Type queryType, HttpQueryAttribute attribute)
         {
@@ -14,7 +16,14 @@ namespace Conqueror
                 return attribute.Path;
             }
 
-            return $"/api/{(attribute.Version > 0 ? $"v{attribute.Version}/" : string.Empty)}queries/{StripSuffixRegex.Replace(queryType.Name, string.Empty)}";
+            // in clients this method may be called repeatedly, and since regex is expensive
+            // we cache the result
+            return PathCache.GetOrAdd(queryType, t =>
+            {
+                var versionPart = attribute.Version > 0 ? $"v{attribute.Version}/" : string.Empty;
+                var namePart = StripSuffixRegex.Replace(t.Name, string.Empty);
+                return $"/api/{versionPart}queries/{namePart}";
+            });
         }
     }
 }
