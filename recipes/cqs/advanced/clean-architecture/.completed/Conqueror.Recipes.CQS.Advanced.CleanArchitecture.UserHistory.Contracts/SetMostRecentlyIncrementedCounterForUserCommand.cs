da860a4192cf2c10daf7427d27300a6b90c2c1ef
Jonathan Ziller <jonathan.ziller@gmail.com>
2023-03-01 21:00:15 +0100

    refactor(recipes.cqs.advanced.clean-architecture): remove superfluous http command annotation

--- recipes/cqs/advanced/clean-architecture/.completed/Conqueror.Recipes.CQS.Advanced.CleanArchitecture.UserHistory.Contracts/SetMostRecentlyIncrementedCounterForUserCommand.cs
+++ recipes/cqs/advanced/clean-architecture/.completed/Conqueror.Recipes.CQS.Advanced.CleanArchitecture.UserHistory.Contracts/SetMostRecentlyIncrementedCounterForUserCommand.cs
@@ -1,6 +1,5 @@
 namespace Conqueror.Recipes.CQS.Advanced.CleanArchitecture.UserHistory.Contracts;
 
-[HttpCommand]
 public sealed record SetMostRecentlyIncrementedCounterForUserCommand([Required] string UserId, [Required] string CounterName);
 
 public interface ISetMostRecentlyIncrementedCounterForUserCommandHandler : ICommandHandler<SetMostRecentlyIncrementedCounterForUserCommand>
