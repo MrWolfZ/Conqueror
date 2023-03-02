97e8d1518d854bff73cc219dbc52fe8fe577521a
Jonathan Ziller <jonathan.ziller@gmail.com>
2023-03-02 23:07:40 +0100

    refactor(recipes.cqs.advanced.clean-architecture): strip project name prefix in command and query logger names

--- recipes/cqs/advanced/clean-architecture/.starting-point/Conqueror.Recipes.CQS.Advanced.CleanArchitecture/DefaultPipelines.cs
+++ recipes/cqs/advanced/clean-architecture/.starting-point/Conqueror.Recipes.CQS.Advanced.CleanArchitecture/DefaultPipelines.cs
@@ -3,10 +3,12 @@ namespace Conqueror.Recipes.CQS.Advanced.CleanArchitecture;
 public static class DefaultPipelines
 {
     public static ICommandPipelineBuilder UseDefault(this ICommandPipelineBuilder pipeline) =>
-        pipeline.UseLogging()
+        // remove the common project prefix from the logger name to reduce the noise in the logs
+        pipeline.UseLogging(o => o.LoggerNameFactory = command => command.GetType().FullName?.Replace("Conqueror.Recipes.CQS.Advanced.CleanArchitecture.", ""))
                 .UseDataAnnotationValidation();
 
     public static IQueryPipelineBuilder UseDefault(this IQueryPipelineBuilder pipeline) =>
-        pipeline.UseLogging()
+        // remove the common project prefix from the logger name to reduce the noise in the logs
+        pipeline.UseLogging(o => o.LoggerNameFactory = query => query.GetType().FullName?.Replace("Conqueror.Recipes.CQS.Advanced.CleanArchitecture.", ""))
                 .UseDataAnnotationValidation();
 }
