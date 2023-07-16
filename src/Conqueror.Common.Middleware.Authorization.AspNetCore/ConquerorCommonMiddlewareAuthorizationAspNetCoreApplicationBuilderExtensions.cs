3bcd4ac24c3d4f0071e371c5d8d114bd077e0807
Jonathan Ziller <jonathan.ziller@gmail.com>
2023-07-16 14:17:43 +0200

    docs(common.middleware.authorization): add code documentation for ASP Core middleware

--- src/Conqueror.Common.Middleware.Authorization.AspNetCore/ConquerorCommonMiddlewareAuthorizationAspNetCoreApplicationBuilderExtensions.cs
+++ src/Conqueror.Common.Middleware.Authorization.AspNetCore/ConquerorCommonMiddlewareAuthorizationAspNetCoreApplicationBuilderExtensions.cs
@@ -5,6 +5,10 @@ namespace Conqueror.Common.Middleware.Authorization.AspNetCore;
 
 public static class ConquerorCommonMiddlewareAuthorizationAspNetCoreApplicationBuilderExtensions
 {
+    /// <summary>
+    ///     Add support for Conqueror authorization to the application. This middleware handles
+    ///     Conqueror authorization failures and converts them to 403 responses.
+    /// </summary>
     public static IApplicationBuilder UseConquerorAuthorization(this IApplicationBuilder app)
     {
         return app.Use(async (ctx, next) =>
