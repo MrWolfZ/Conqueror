cbc9f77673a9818402beca653194a46301d0e399
Jonathan Ziller <jonathan.ziller@gmail.com>
2023-07-16 14:16:12 +0200

    docs(common.middleware.authentication): add code documentation for ASP Core middleware

--- src/Conqueror.Common.Middleware.Authentication.AspNetCore/ConquerorCommonMiddlewareAuthenticationAspNetCoreApplicationBuilderExtensions.cs
+++ src/Conqueror.Common.Middleware.Authentication.AspNetCore/ConquerorCommonMiddlewareAuthenticationAspNetCoreApplicationBuilderExtensions.cs
@@ -1,11 +1,19 @@
-using Microsoft.AspNetCore.Authentication;
+﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Conqueror.Common.Middleware.Authentication.AspNetCore;
 
 public static class ConquerorCommonMiddlewareAuthenticationAspNetCoreApplicationBuilderExtensions
 {
+    /// <summary>
+    ///     Add support for Conqueror authentication to the application. This middleware propagates the
+    ///     ASP Core user (see <see cref="HttpContext.User" />) to the Conqueror authentication infrastructure.
+    ///     It also handles Conqueror authentication failures and converts them to 401 responses.<br />
+    ///     <br />
+    ///     Note that this middleware MUST be placed after <see cref="AuthAppBuilderExtensions.UseAuthentication" />.
+    /// </summary>
     public static IApplicationBuilder UseConquerorAuthentication(this IApplicationBuilder app)
     {
         return app.Use(async (ctx, next) =>
