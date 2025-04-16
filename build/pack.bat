dfc91848653965aa721f390645ca01adcef7858d
Jonathan Ziller <jonathan.ziller@gmail.com>
2025-04-16 14:06:47 +0200

    build: remove obsolete packages from publishing

--- build/pack.bat
+++ build/pack.bat
@@ -19,40 +19,20 @@ REM pack
 echo packing projects...
 
 mkdir .publish
-dotnet publish %srcDir%\Conqueror.Transport.Http.SourceGenerators -c Release --framework netstandard2.0 -o %publishDir%/Conqueror.Transport.Http.SourceGenerators
-dotnet pack %srcDir%\Conqueror.Common.Middleware.Authentication.Abstractions -c Release -o %publishDir% --include-symbols
-dotnet pack %srcDir%\Conqueror.Common.Middleware.Authorization.Abstractions -c Release -o %publishDir% --include-symbols
-dotnet pack %srcDir%\Conqueror.Common.Transport.Http.Abstractions -c Release -o %publishDir% --include-symbols
-dotnet pack %srcDir%\Conqueror.Common.Transport.Http.Server.AspNetCore -c Release -o %publishDir% --include-symbols
-dotnet pack %srcDir%\Conqueror.CQS -c Release -o %publishDir% --include-symbols
-dotnet pack %srcDir%\Conqueror.CQS.Abstractions -c Release -o %publishDir% --include-symbols
-dotnet pack %srcDir%\Conqueror.CQS.Analyzers -c Release -o %publishDir% --include-symbols
-dotnet pack %srcDir%\Conqueror.CQS.Middleware.Authentication -c Release -o %publishDir% --include-symbols
-dotnet pack %srcDir%\Conqueror.CQS.Middleware.Authorization -c Release -o %publishDir% --include-symbols
-dotnet pack %srcDir%\Conqueror.CQS.Middleware.DataAnnotationValidation -c Release -o %publishDir% --include-symbols
-dotnet pack %srcDir%\Conqueror.CQS.Middleware.Logging -c Release -o %publishDir% --include-symbols
-dotnet pack %srcDir%\Conqueror.CQS.Middleware.Polly -c Release -o %publishDir% --include-symbols
-dotnet pack %srcDir%\Conqueror.CQS.Transport.Http.Abstractions -c Release -o %publishDir% --include-symbols
-dotnet pack %srcDir%\Conqueror.CQS.Transport.Http.Common -c Release -o %publishDir% --include-symbols
-dotnet pack %srcDir%\Conqueror.CQS.Transport.Http.Client -c Release -o %publishDir% --include-symbols
-dotnet pack %srcDir%\Conqueror.CQS.Transport.Http.Server.AspNetCore -c Release -o %publishDir% --include-symbols
-dotnet pack %srcDir%\Conqueror.Eventing -c Release -o %publishDir% --include-symbols
-dotnet pack %srcDir%\Conqueror.Eventing.Abstractions -c Release -o %publishDir% --include-symbols
-dotnet pack %srcDir%\Conqueror.Streaming -c Release -o %publishDir% --include-symbols
-dotnet pack %srcDir%\Conqueror.Streaming.Abstractions -c Release -o %publishDir% --include-symbols
-dotnet pack %srcDir%\Conqueror.Streaming.Transport.Http.Abstractions -c Release -o %publishDir% --include-symbols
-dotnet pack %srcDir%\Conqueror.Streaming.Transport.Http.Common -c Release -o %publishDir% --include-symbols
-dotnet pack %srcDir%\Conqueror.Streaming.Transport.Http.Client -c Release -o %publishDir% --include-symbols
-dotnet pack %srcDir%\Conqueror.Streaming.Transport.Http.Server.AspNetCore -c Release -o %publishDir% --include-symbols
-dotnet pack %srcDir%\Conqueror.Middleware.Logging -c Release -o %publishDir% --include-symbols
-dotnet pack %srcDir%\Conqueror.Transport.Http.Abstractions -c Release -o %publishDir% --include-symbols
-dotnet pack %srcDir%\Conqueror.Transport.Http.Client -c Release -o %publishDir% --include-symbols
-dotnet pack %srcDir%\Conqueror.Transport.Http.Server.AspNetCore -c Release -o %publishDir% --include-symbols
 dotnet pack %srcDir%\Conqueror.SourceGenerators.Util -c Release -o %publishDir% --include-symbols
 dotnet pack %srcDir%\Conqueror.SourceGenerators.TestUtil -c Release -o %publishDir% --include-symbols
 dotnet publish %srcDir%\Conqueror.SourceGenerators -c Release --framework netstandard2.0 -o %publishDir%/Conqueror.SourceGenerators
 dotnet pack %srcDir%\Conqueror.Abstractions -c Release -o %publishDir% --include-symbols
 dotnet pack %srcDir%\Conqueror -c Release -o %publishDir% --include-symbols
+
+REM middlewares
+dotnet pack %srcDir%\Conqueror.Middleware.Logging -c Release -o %publishDir% --include-symbols
+
+REM transports
+dotnet publish %srcDir%\Conqueror.Transport.Http.SourceGenerators -c Release --framework netstandard2.0 -o %publishDir%/Conqueror.Transport.Http.SourceGenerators
+dotnet pack %srcDir%\Conqueror.Transport.Http.Abstractions -c Release -o %publishDir% --include-symbols
+dotnet pack %srcDir%\Conqueror.Transport.Http.Client -c Release -o %publishDir% --include-symbols
+dotnet pack %srcDir%\Conqueror.Transport.Http.Server.AspNetCore -c Release -o %publishDir% --include-symbols
 
 REM post-clean
 
