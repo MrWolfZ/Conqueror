6e3a87436759676cbbda30758f63d98e00a3bf47
Jonathan Ziller <jonathan.ziller@gmail.com>
2025-04-27 14:41:08 +0200

    test(source-generators): add more test cases

---
+++ src/Conqueror.SourceGenerators.Tests/Signalling/TestCases/WithCustomTransportWithHierarchy/source.cs
@@ -0,0 +1,51 @@
+﻿#nullable enable
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Conqueror.Signalling;
+
+namespace Conqueror.SourceGenerators.Tests.Signalling.TestCases.WithCustomTransportWithHierarchy;
+
+[SignalTransport(Prefix = "TestTransport", Namespace = "Conqueror.SourceGenerators.Tests.Signalling.TestCases.WithCustomTransportWithHierarchy")]
+[AttributeUsage(AttributeTargets.Class, Inherited = false)]
+public sealed class TestTransportSignalAttribute : Attribute;
+
+public interface ITestTransportSignal<out TSignal> : ISignal<TSignal>
+    where TSignal : class, ITestTransportSignal<TSignal>;
+
+public interface ITestTransportSignalHandler<TSignal, TIHandler>
+    where TSignal : class, ITestTransportSignal<TSignal>
+    where TIHandler : class, ITestTransportSignalHandler<TSignal, TIHandler>
+{
+    static ISignalHandlerTypesInjector CreateTestTransportTypesInjector<THandler>()
+        where THandler : class, TIHandler
+        => throw new NotSupportedException();
+}
+
+[TestTransportSignal]
+public abstract partial record TestSignal(int Payload);
+
+[TestTransportSignal]
+public sealed partial record TestSignalSub(int Payload) : TestSignal(Payload);
+
+public partial class TestSignalHandler : TestSignal.IHandler
+{
+    public Task Handle(TestSignal message, CancellationToken cancellationToken) => throw new NotSupportedException();
+}
+
+public partial class TestSignalSubHandler : TestSignalSub.IHandler
+{
+    public Task Handle(TestSignalSub message, CancellationToken cancellationToken) => throw new NotSupportedException();
+}
+
+// make the compiler happy during design time
+public partial record TestSignal
+{
+    public partial interface IHandler;
+}
+
+public partial record TestSignalSub
+{
+    public new partial interface IHandler;
+}
