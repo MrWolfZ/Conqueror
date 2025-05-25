808da53a96f4215d9b69a1bbf2873e747fa20236
Jonathan Ziller <jonathan.ziller@gmail.com>
2025-05-25 10:56:43 +0200

    refactor(transport.http.signalling): disable response buffering

--- src/transports/http/Conqueror.Transport.Http.Server.AspNetCore/Signalling/Sse/ConquerorHttpSseServerSignallingEndpointRouteBuilderExtensions.cs
+++ src/transports/http/Conqueror.Transport.Http.Server.AspNetCore/Signalling/Sse/ConquerorHttpSseServerSignallingEndpointRouteBuilderExtensions.cs
@@ -1,13 +1,7 @@
-using System;
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.IO;
 using System.Linq;
-using System.Net.ServerSentEvents;
-using System.Runtime.CompilerServices;
-using System.Threading;
-using System.Threading.Tasks;
 using Conqueror;
+using Conqueror.Transport.Http.Server.AspNetCore;
 using Conqueror.Transport.Http.Server.AspNetCore.Signalling.Sse;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
@@ -20,8 +14,10 @@ namespace Microsoft.Extensions.DependencyInjection;
 
 public static class ConquerorHttpSseServerSignallingEndpointRouteBuilderExtensions
 {
-    [SuppressMessage("Minor Code Smell", "S6667:Logging in a catch clause should pass the caught exception as a parameter.",
-                     Justification = "not necessary for cancellation exceptions")]
+    [SuppressMessage(
+        "Minor Code Smell",
+        "S6667:Logging in a catch clause should pass the caught exception as a parameter.",
+        Justification = "not necessary for cancellation exceptions")]
     public static IEndpointConventionBuilder MapServerSentEventsSignalsEndpoint(this IEndpointRouteBuilder builder, string path)
     {
         return builder.MapGet(
@@ -44,52 +40,14 @@ public static class ConquerorHttpSseServerSignallingEndpointRouteBuilderExtensio
                     return;
                 }
 
-                try
-                {
-                    var singletons = context.RequestServices.GetRequiredService<ConquerorSingletons>();
-                    var broker = singletons.GetOrAddSingleton(p => new HttpSseSignalBroker(p));
-
-                    var items = broker.Subscribe(eventTypes, context.RequestAborted);
-
-                    context.Response.StatusCode = StatusCodes.Status200OK;
-                    context.Response.ContentType = ContentTypes.EventStream;
-
-                    // flush the response stream to ensure that the client receives the headers
-                    await context.Response.Body.FlushAsync(context.RequestAborted).ConfigureAwait(false);
-
-                    await SseFormatter.WriteAsync(
-                                          RunWithFlushing(items, context.Response.Body, context.RequestAborted),
-                                          context.Response.Body,
-                                          context.RequestAborted)
-                                      .ConfigureAwait(false);
-
-                    static async IAsyncEnumerable<SseItem<string>> RunWithFlushing(
-                        IAsyncEnumerable<SseItem<string>> items,
-                        Stream responseBody,
-                        [EnumeratorCancellation] CancellationToken ct)
-                    {
-                        await foreach (var item in items.ConfigureAwait(false).WithCancellation(ct))
-                        {
-                            yield return item;
-
-                            // flush the response body after each item (which unfortunately isn't done automatically
-                            // in the SseFormatter
-                            await responseBody.FlushAsync(ct).ConfigureAwait(false);
-                        }
-                    }
-                }
-                catch (Exception) when (context.RequestAborted.IsCancellationRequested)
-                {
-                    // nothing to do, the client just disconnected, which may have thrown spurious exceptions
-                    logger.LogInformation("client disconnected from signal stream");
-                }
-                catch (Exception e)
-                {
-                    // we don't handle the error here since terminating the connection should cause the client to reconnect;
-                    // any missed signals due to this are by the nature of this transport which does not buffer anything and
-                    // provides no guarantees about receiving all signals
-                    logger.LogError(e, "an error occurred while formatting signal stream");
-                }
+                await SseEndpoint.Run(
+                                     context,
+                                     logger,
+                                     () => context.RequestServices
+                                                  .GetRequiredService<ConquerorSingletons>()
+                                                  .GetOrAddSingleton(p => new HttpSseSignalBroker(p))
+                                                  .Subscribe(eventTypes, context.RequestAborted))
+                                 .ConfigureAwait(false);
             });
     }
 }
---
+++ src/transports/http/Conqueror.Transport.Http.Server.AspNetCore/SseEndpoint.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Net.ServerSentEvents;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.Extensions.Logging;
+using static Conqueror.ConquerorTransportHttpConstants;
+
+namespace Conqueror.Transport.Http.Server.AspNetCore;
+
+internal static class SseEndpoint
+{
+    [SuppressMessage(
+        "Minor Code Smell",
+        "S6667:Logging in a catch clause should pass the caught exception as a parameter.",
+        Justification = "not necessary for cancellation exceptions")]
+    public static async Task Run(
+        HttpContext context,
+        ILogger logger,
+        Func<IAsyncEnumerable<SseItem<string>>> getItems)
+    {
+        try
+        {
+            var items = getItems();
+
+            if (context.Features.Get<IHttpResponseBodyFeature>() is { } f)
+            {
+                f.DisableBuffering();
+            }
+
+            context.Response.StatusCode = StatusCodes.Status200OK;
+            context.Response.ContentType = ContentTypes.EventStream;
+
+            // flush the response stream to ensure that the client receives the headers
+            await context.Response.Body.FlushAsync(context.RequestAborted).ConfigureAwait(false);
+
+            await SseFormatter.WriteAsync(
+                                  RunWithFlushing(items, context.Response.Body, context.RequestAborted),
+                                  context.Response.Body,
+                                  context.RequestAborted)
+                              .ConfigureAwait(false);
+
+            static async IAsyncEnumerable<SseItem<string>> RunWithFlushing(
+                IAsyncEnumerable<SseItem<string>> items,
+                Stream responseBody,
+                [EnumeratorCancellation] CancellationToken ct)
+            {
+                await foreach (var item in items.ConfigureAwait(false).WithCancellation(ct))
+                {
+                    yield return item;
+
+                    // flush the response body after each item (which unfortunately isn't done automatically
+                    // in the SseFormatter
+                    await responseBody.FlushAsync(ct).ConfigureAwait(false);
+                }
+            }
+        }
+        catch (Exception) when (context.RequestAborted.IsCancellationRequested)
+        {
+            // nothing to do, the client just disconnected, which may have thrown spurious exceptions
+            logger.LogInformation("client disconnected from event stream");
+        }
+        catch (Exception e)
+        {
+            // we don't handle the error here since terminating the connection should cause the client to reconnect;
+            // any missed items due to this are by the nature of this transport which does not buffer anything and
+            // provides no guarantees about receiving all items
+            logger.LogError(e, "an error occurred while formatting event stream");
+        }
+    }
+}
