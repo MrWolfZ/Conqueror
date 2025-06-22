0cfaf83584bc76b4da5e61b9edc22f37dbe7a792
Jonathan Ziller <jonathan.ziller@gmail.com>
2025-06-22 11:41:15 +0200

    feat(benchmarks): add logging middleware benchmarks

---
+++ src/Conqueror.Benchmarks/MessageLoggingMiddlewareBenchmarks.cs
@@ -0,0 +1,98 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Conqueror.Benchmarks;
+
+[Config(typeof(ConfigWithCustomEnvVars))]
+[MemoryDiagnoser]
+[SuppressMessage("ReSharper", "ClassCanBeSealed.Global", Justification = "Benchmark.NET requires non-sealed classes")]
+public partial class MessageLoggingMiddlewareBenchmarks
+{
+    [Benchmark]
+    [ArgumentsSource(nameof(Arguments))]
+    public void Run(int numOfExecutions, int? parallelism)
+    {
+        var serviceProvider = new ServiceCollection().AddMessageHandler<TestMessageHandler>()
+                                                     .AddLogging(l => l.SetMinimumLevel(LogLevel.None))
+                                                     .BuildServiceProvider();
+
+        Run(RunSingle, numOfExecutions, parallelism).GetAwaiter().GetResult();
+
+        async ValueTask RunSingle(int idx)
+        {
+            var response = await serviceProvider.GetRequiredService<IMessageSenders>()
+                                                .For(TestMessage.T)
+                                                .WithPipeline(static pipeline => pipeline.UseLogging(c => c.StackTraceCaptureIsDisabled = true))
+                                                .Handle(new(idx));
+
+            if (response.Value != idx)
+            {
+                throw new InvalidOperationException($"got wrong result {response.Value} on execution {idx}, expected {idx}");
+            }
+        }
+    }
+
+    public static IEnumerable<object?[]> Arguments()
+    {
+        foreach (var (numOfExecutions, parallelism) in from numOfExecutions in new[] { 1, 100, 1_000, 10_000, 100_000 }
+                                                       from parallelism in new int?[] { null, 4 }
+                                                       where parallelism is null || numOfExecutions >= parallelism
+                                                       select (numOfExecutions, parallelism))
+        {
+            yield return [numOfExecutions, parallelism];
+        }
+    }
+
+    private static async Task Run(Func<int, ValueTask> runSingle, int numOfExecutions, int? parallelism)
+    {
+        if (parallelism is not null)
+        {
+            await Parallel.ForEachAsync(
+                Enumerable.Range(0, numOfExecutions),
+                new ParallelOptions { MaxDegreeOfParallelism = parallelism.Value },
+                (i, _) => runSingle(i));
+
+            return;
+        }
+
+        for (var i = 0; i < numOfExecutions; i += 1)
+        {
+            await runSingle(i);
+        }
+    }
+
+    // in case we want to test different implementations, we can toggle them with environment variables
+    private sealed class ConfigWithCustomEnvVars : ManualConfig
+    {
+        // ReSharper disable once EmptyConstructor
+        public ConfigWithCustomEnvVars()
+        {
+            AddJob(Job.ShortRun);
+
+            // AddJob(Job.Default
+            //           .WithEnvironmentVariables(new EnvironmentVariable("SOME_VAR", "SOME_VALUE"))
+            //           .WithId("some ID"));
+        }
+    }
+
+    [Message<TestMessageResponse>]
+    private sealed partial record TestMessage(int Value);
+
+    private sealed record TestMessageResponse(int Value);
+
+    private sealed partial class TestMessageHandler : TestMessage.IHandler
+    {
+        public async Task<TestMessageResponse> Handle(TestMessage query, CancellationToken cancellationToken = new())
+        {
+            await Task.Yield();
+
+            return new(query.Value);
+        }
+
+        public static void ConfigurePipeline(TestMessage.IPipeline pipeline) => pipeline.UseLogging(static c => c.StackTraceCaptureIsDisabled = true);
+    }
+}
--- src/Conqueror.Benchmarks/Program.cs
+++ src/Conqueror.Benchmarks/Program.cs
@@ -2,6 +2,7 @@
 using BenchmarkDotNet.Running;
 using Conqueror.Benchmarks;
 
+// ReSharper disable UnreachableSwitchCaseDueToIntegerAnalysis
 // ReSharper disable once UnreachableSwitchCaseDueToIntegerAnalysis
 
 var toRun = 1;
@@ -9,17 +10,30 @@ var toRun = 1;
 switch (toRun)
 {
     case 1:
-        _ = BenchmarkRunner.Run(typeof(Program).Assembly);
+        _ = BenchmarkRunner.Run<MessageBenchmarks>();
+
         break;
 
     case 2:
-        Run();
-        break;
-}
+    {
+        var sw = Stopwatch.StartNew();
+        new MessageBenchmarks().RunWithConqueror(100_000, 16, 20);
+        Console.WriteLine(sw.Elapsed);
 
-void Run()
-{
-    var sw = Stopwatch.StartNew();
-    new MessageBenchmarks().RunWithConqueror(100_000, 16, 20);
-    Console.WriteLine(sw.Elapsed);
+        break;
+    }
+
+    case 3:
+        _ = BenchmarkRunner.Run<MessageLoggingMiddlewareBenchmarks>();
+
+        break;
+
+    case 4:
+    {
+        var sw = Stopwatch.StartNew();
+        new MessageLoggingMiddlewareBenchmarks().Run(100_000, 16);
+        Console.WriteLine(sw.Elapsed);
+
+        break;
+    }
 }
