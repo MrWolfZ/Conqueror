d37700b31eb2eb8f8b2bc74dde09370777b6b0cb
Jonathan Ziller <jonathan.ziller@gmail.com>
2025-07-14 14:25:39 +0200

    test(transport.file-system): increase test timeouts

--- src/transports/file-system/Conqueror.Transport.FileSystem.Tests/FileSystemTransportTestTimeouts.cs
+++ src/transports/file-system/Conqueror.Transport.FileSystem.Tests/FileSystemTransportTestTimeouts.cs
@@ -26,11 +26,11 @@ public sealed class FileSystemTransportTestTimeouts : IDisposable
     {
         var assertionTimeout = Debugger.IsAttached
             ? TimeSpan.FromMinutes(1)
-            : TimeSpan.FromMilliseconds(IsRunningInGithubActionField ? 10_000 : 2_000);
+            : TimeSpan.FromMilliseconds(IsRunningInGithubActionField ? 10_000 : 3_000);
 
         var testHost = new FileSystemTransportTestTimeouts
         {
-            TestTimeout = testTimeout ?? TimeSpan.FromSeconds(IsRunningInGithubActionField ? 30 : 3),
+            TestTimeout = testTimeout ?? TimeSpan.FromSeconds(IsRunningInGithubActionField ? 30 : 5),
             AssertionTimeoutInMs = (int)assertionTimeout.TotalMilliseconds,
             ShortDelay = TimeSpan.FromMilliseconds(IsRunningInGithubActionField ? 2_000 : 200),
         };
